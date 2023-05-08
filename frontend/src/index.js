17b7c76317e8ac9f412b6c3e56a57ef7fd8a5a34
alex5 <alex@kimnetics.com>
2023-05-08 13:24:28 -1000

    working auth1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+// frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,12 +6,19 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf'; // from alex
+import * as sessionActions from "./store/session"
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
+
 
 function Root() {
   return (
