83edd82579075f28ae155a9824934767dbcc9dd5
alex5 <alex@kimnetics.com>
2023-05-13 22:16:38 -1000

    yikes

--- frontend/src/components/SpotList/SpotList.js
+++ frontend/src/components/SpotList/SpotList.js
@@ -8,6 +8,7 @@ const SpotList = ({ showManageOptions }) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const spots = useSelector((state) => state.spots.allSpots);
+
   useEffect(() => {
     dispatch(fetchAllSpots());
   }, [dispatch]);
@@ -23,39 +24,37 @@ const SpotList = ({ showManageOptions }) => {
   };
 
   return (
-    <div>
+    <div className="spot-list">
       <h2>Spot List</h2>
-      <div className="spot-list">
-        <ul className="spot-ul">
-          {spots &&
-            Object.values(spots).map((spot) => (
-              <li
-                key={spot.id}
-                title={spot.name} // Tooltip
-                onClick={(e) => handleSpotClick(spot.id, e)}
-                className="spot-tile"
-              >
-                {/* <h3>{spot.name}</h3> */}
-                <img src={spot.preview_image} alt="Preview" />
-                <p>{spot.description}</p>
-                <div className="below_image">
-                  <div className="city_state"> {spot.city}, {spot.state}
-                      <div className="rating">{spot.avg_rating ? (<><span role="img" aria-label="star">🌟</span> {spot.avg_rating}</>) : ("New")}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="price"> ${spot.price} night
-                </div>
-                {showManageOptions && (
-                  <div>
-                    <button className="manage-button" onClick={() => history.push(`/spots/${spot.id}/edit`)}>Update</button>
-                    <button className="manage-button" onClick={() => handleDelete(spot.id)}>Delete</button>
-                  </div>
-                )}
-              </li>
-            ))}
-        </ul>
-      </div>
+      <ul className="spot-ul">
+        {spots && Object.values(spots).map((spot) => (
+          <li
+            key={spot.id}
+            title={spot.name}
+            onClick={(e) => handleSpotClick(spot.id, e)}
+            className="spot-tile"
+          >
+            <img src={spot.preview_image} alt="Preview" />
+            <p className="spot_description">{spot.description}</p>
+            <div className="below_image">
+              <div className="city_state">{spot.city}, {spot.state}</div>
+              <div className="rating">
+                {spot.avg_rating
+                  ? (<><span role="img" aria-label="star">🌟</span> {spot.avg_rating}</>)
+                  : ("New")
+                }
+              </div>
+            </div>
+            <div className="price">${spot.price} / night</div>
+            {showManageOptions && (
+              <div className="manage-options">
+                <button className="manage-button" onClick={() => history.push(`/spots/${spot.id}/edit`)}>Update</button>
+                <button className="manage-button" onClick={() => handleDelete(spot.id)}>Delete</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
